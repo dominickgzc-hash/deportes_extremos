179dfadeaa52aed165c882d53bffa4cb04976d8f
dominickgzc-hash <dominickgzc@gmail.com>
2026-04-16 10:11:40 -0600

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Aquí puedes agregar código JavaScript si es necesario
+    console.log("Página cargada. Listo para interactuar.");
+});
